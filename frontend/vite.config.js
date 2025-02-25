394abac90224e33e2d0691d952ee830e123f9a84
Kamalesh124 <k21370794@gmail.com>
2025-02-26 00:49:10 +0530

    Initial Commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    host: true,
+    port: 5173, // or your preferred port
+  }
+})
